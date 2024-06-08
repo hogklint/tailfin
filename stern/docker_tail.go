025e4c77995a5641073b7cfdb4fd4f4ec33b15ff
Jimmie Högklint <hogklint@gmail.com>
2024-06-08 23:47:33 +0200

    Some separation that is likely not very useful

--- stern/docker_tail.go
+++ stern/docker_tail.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"io"
 	//"strings"
+	"sync"
 	"time"
 
 	"github.com/docker/docker/api/types"
@@ -13,31 +14,53 @@ import (
 	"github.com/docker/docker/client"
 )
 
-func StartDocker() {
+func StartDocker(config *Config) {
 	apiClient, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
 	if err != nil {
 		panic(err)
 	}
 	defer apiClient.Close()
 
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		err := findContainers(config, apiClient)
+		if err != nil {
+			fmt.Fprintf(config.ErrOut, "Some error occured 1234: %v\n", err)
+			return
+		}
+	}()
+	wg.Wait()
+}
+
+func findContainers(config *Config, apiClient *client.Client) error {
 	knownContainers := make(map[string]bool)
 	opts := container.ListOptions{All: true}
 	for {
 		containers, err := apiClient.ContainerList(context.Background(), opts)
 		if err != nil {
-			panic(err)
+			return err
 		}
 
 		newContainers := make(map[string]bool)
 		for _, ctr := range containers {
 			//fmt.Printf("%s %s (status: %s)\n", ctr.ID, ctr.Image, ctr.Status)
 			if _, ok := knownContainers[ctr.ID]; !ok {
-				go Start(apiClient, ctr)
+				go startTail(config, apiClient, ctr)
 			}
 			newContainers[ctr.ID] = true
 		}
 		knownContainers = newContainers
 		time.Sleep(1 * time.Second)
+	}
+}
+
+func startTail(config *Config, apiClient *client.Client, container types.Container) {
+	err := Start(apiClient, container)
+	if err != nil {
+		fmt.Fprintf(config.ErrOut, "Some error occured 4567: %v\n", err)
+		return
 	}
 }
 
